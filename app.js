442c8008627ea2efbdf93c23b72ceb3a08a9d86d
Judith Ramírez <judithramirezpachon@gmail.com>
2025-05-28 10:04:07 +0000

    generador dominio

---
+++ app.js
@@ -0,0 +1,16 @@
+import "bootstrap";
+import "./style.css";
+
+
+let pronouns = ['la', 'nuestra', 'tu'];
+let adjs = ['gran', 'pequeña', 'bonita'];
+let nouns = ['ciudad', 'aldea', 'villa'];
+
+for (let pronoun of pronouns) {
+  for (let adj of adjs) {
+    for (let noun of nouns) {
+      let domains = pronoun + adj + noun;
+       console.log("www." + domains + ".es");
+    }
+  }
+}
